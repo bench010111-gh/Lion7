9950dea45041832c27608b816dddefa5d5980514
bench010111 <22000748@handong.ac.kr>
2026-01-05 10:14:31 +0900

    first commit

---
+++ C# 101/01_console_game.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Runtime.ConstrainedExecution;
+using System.Security.Cryptography;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("        ┌──────────────────────────────────────────────────────────────────────────┬──────────────────────────────────────────────┐");
+            System.Console.WriteLine("        │        추천               모든 아이템            아이템 세트             │                                           x  │");
+            System.Console.WriteLine("        ├──────────────────────────────────────────────────────────────────────────┤                                              │");
+            System.Console.WriteLine("        │ 1.  무한의 대검          2.  요우무의 유령검     3.  스태틱의 단검      │   4.  광전사의 군화                        │");
+            System.Console.WriteLine("        └──────────────────────────────────────────────────────────────────────────┴──────────────────────────────────────────────┘");
+
+        }
+    }
+}
