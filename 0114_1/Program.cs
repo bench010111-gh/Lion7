8ec2e82da54b8c60d2614d6ba941ebe2ce289aa4
bench010111-gh <benjaminhcoi7@gmail.com>
2026-01-14 17:13:36 +0900

    0114 commit

---
+++ 0114_1/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// inheritance
+
+namespace _0114_1
+{
+    // parent class
+    class Character
+    {
+        // public, private, protected(allow usage by child)
+        protected string name = "";
+        protected int level;
+        protected int hp;
+        protected int maxHP;
+        protected int attack;
+        protected int defense;
+
+        public Character(string characterName)
+        {
+            name = characterName;
+            level = 1;
+            maxHP = 1;
+            hp = maxHP;
+            attack = 30;
+            defense = 20;
+
+            Console.WriteLine($"Character {name} created!");
+        }
+
+        public virtual void ShowInfo()
+        {
+            Console.WriteLine($"Name : {name}");
+            Console.WriteLine($"Level : {level}");
+            Console.WriteLine($"HP : {hp}/{maxHP}");
+            Console.WriteLine($"Attack : {attack}");
+            Console.WriteLine($"Defense : {defense}");
+        }
+    }
+
+    class Warrior : Character
+    {
+        private int rage; // warrior-unique attribute
+
+        public Warrior(string name) : base(name) // put name parameter to parent's constructr
+        {
+            // name = "Warrior";   // same as base.name
+            attack = 60;
+            defense = 40;
+            maxHP = 150;
+            hp = maxHP;
+            rage = 0;
+
+            Console.WriteLine("Job : Warrior");
+        }
+
+        public override void ShowInfo()
+        {
+            // base.ShowInfo();    // can function call parent function
+            Console.WriteLine($"Rage : {rage}");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Character character = new Character("Peter");
+            // character.ShowInfo();
+
+            // Warrior warrior = new Warrior("John");
+            // warrior.ShowInfo();
+
+            Character character = new Warrior("PJ");
+            character.ShowInfo();
+
+            // inheritance relationship
+            // child calling: parent constructor, then child constructor
+            //                  child destructor, then parent destructor
+
+            // how to intialize parent member via child member
+            
+        }
+    }
+}
---
+++ 0114_2/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// inheritance
+
+namespace _0114_2
+{
+        // 문제 2: 상점 시스템
+        // 다양한 상품 타입을 다형성으로 관리하고,
+        // 타입별로 다른 할인율을 적용하세요.
+
+    class Item    // parent class
+    {
+        public string Name;
+        public int Discount;
+
+        public Item (string name)
+        {
+            Name = name;
+            Discount = 0;
+        }
+
+        public virtual void Desc()
+        {
+            Console.WriteLine($"This is an item, and is on {Discount}% sale");
+        }
+
+        public virtual void ApplyDiscount()
+        {
+            Discount = 0;
+        }
+    }
+
+    class Dairies : Item  // child class
+    {
+        public Dairies (string name) : base(name) {}
+
+        public override void Desc()
+        {
+            Console.WriteLine($"{Name}: This is a dairy item, and is on {Discount}% sale");
+        }
+
+        public override void ApplyDiscount()
+        {
+            Discount = 30;
+        }
+    }
+
+    class Meat : Item
+    {
+        public Meat (string name) : base(name) {}
+
+        public override void Desc()
+        {
+            Console.WriteLine($"{Name}: This is a meat item, and is on {Discount}% sale");
+        }
+
+        public override void ApplyDiscount()
+        {
+            Discount = 10;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Item[] items = new Item[4];
+
+            items[0] = new Dairies("Milk");
+            items[1] = new Dairies("Cheese");
+            items[2] = new Meat("Chicken");
+            items[3] = new Meat("Pork");
+
+            for (int i = 0; i < items.Length; i++)
+            {
+                items[i].ApplyDiscount();
+                items[i].Desc();
+            }
+        }
+    }
+}
